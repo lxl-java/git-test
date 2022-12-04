3ec842daf353eeb5507ca15ebcf0abad53e2a5db
lxl <962090671@qq.com>
2022-12-04 22:30:56 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @Date 2022/12/4
+ * @Author lxl
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
